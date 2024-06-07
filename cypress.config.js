250669ad505c41887a6bc8a65e0323aefd44245e
Fernanda Fonseca <fernanda_fonseca0107@hotmail.com>
2024-06-06 23:10:56 -0300

    Mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'xehgs8',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     
       baseUrl: 'http://lojaebac.ebaconline.art.br/',
+      video:true,
     },
 
 });
